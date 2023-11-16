80e3c9231a4dd92fb58abfb71d4b38ada6696ffc
Ben Westgate <BenWestgate@protonmail.com>
2023-11-16 11:11:04 -0600

    Revert "Update lib.py"
    This reverts commit 6b4203c902236a6cd7c774a04f74546d4ac03440.

--- reference/python-codex32/src/lib.py
+++ reference/python-codex32/src/lib.py
@@ -167,9 +167,11 @@ def ms32_decode(ms32_str):
         return None, None, None, None, None
     hrp = ms32_str[:pos]
     k = ms32_str[pos + 1]
+    if not k.isdigit():
+        return None, None, None, None, None
     ident = ms32_str[pos + 2: pos + 6]
     share_index = ms32_str[pos + 6]
-    if not k.isdigit() or k == "0" and share_index != "s":
+    if k == "0" and share_index != "s":
         return None, None, None, None, None
     data = [CHARSET.find(x) for x in ms32_str[pos + 1:]]
     checksum_length = 13 if len(data) < 95 else 15
